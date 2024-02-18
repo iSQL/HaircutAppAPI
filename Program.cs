087ced9aa97a61d3f80a7a1f9ab47aa0211a590c
iSQL <maremilojkovic@gmail.com>
2024-02-18 11:28:45 +0100

    - Added API key auth

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
+using HaircutAppAPI.Security;
 using HaircutAppAPI.Services;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -15,7 +17,38 @@ builder.Services.AddSingleton<CosmosDbContext>(s => new CosmosDbContext(cosmosCo
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+	c.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" });
+
+	// Define the OAuth2.0 scheme that's being used (i.e., bearer token, JWT)
+	c.AddSecurityDefinition("ApiKey", new OpenApiSecurityScheme
+	{
+		Description = "API Key needed to access the endpoints. X-API-Key: YOUR_API_KEY",
+		In = ParameterLocation.Header,
+		Name = "X-API-Key", // Name of the header to be used
+		Type = SecuritySchemeType.ApiKey,
+		Scheme = "ApiKeyScheme"
+	});
+
+	c.AddSecurityRequirement(new OpenApiSecurityRequirement
+		{
+			{
+				new OpenApiSecurityScheme
+				{
+					Reference = new OpenApiReference
+					{
+						Type = ReferenceType.SecurityScheme,
+						Id = "ApiKey"
+					},
+					Scheme = "ApiKeyScheme",
+					Name = "ApiKey",
+					In = ParameterLocation.Header,
+				},
+				Array.Empty<string>()
+			}
+		});
+});
 
 var app = builder.Build();
 
@@ -23,12 +56,18 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
 	app.UseSwagger();
-	app.UseSwaggerUI();
+	app.UseSwaggerUI(c =>
+	{
+		c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+		// To serve Swagger UI at application's root page, set the RoutePrefix property to an empty string
+		c.RoutePrefix = string.Empty;
+	});
 }
 
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
+app.UseMiddleware<ApiKeyAuthenticationMiddleware>();
 
 app.MapControllers();
 
---
+++ Security/ApiKeyAuthenticationMiddleware.cs
@@ -0,0 +1,35 @@
+﻿namespace HaircutAppAPI.Security
+{
+	public class ApiKeyAuthenticationMiddleware
+	{
+		private readonly RequestDelegate _next;
+		private const string ApiKeyHeaderName = "X-API-Key";
+
+		public ApiKeyAuthenticationMiddleware(RequestDelegate next)
+		{
+			_next = next;
+		}
+
+		public async Task Invoke(HttpContext context)
+		{
+			if (!context.Request.Headers.TryGetValue(ApiKeyHeaderName, out var extractedApiKey))
+			{
+				context.Response.StatusCode = 401; // Unauthorized
+				await context.Response.WriteAsync("API Key is missing");
+				return;
+			}
+
+			var appSettings = context.RequestServices.GetRequiredService<IConfiguration>();
+			var apiKey = appSettings.GetValue<string>("ApiKey");
+
+			if (!apiKey.Equals(extractedApiKey))
+			{
+				context.Response.StatusCode = 401; // Unauthorized
+				await context.Response.WriteAsync("Unauthorized client");
+				return;
+			}
+
+			await _next(context);
+		}
+	}
+}
